6d71522499462520e82071c825e71f7884a474f9
kabeerx9 <kabeer786joshi@gmail.com>
2023-12-17 21:16:40 +0530

    Log in

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,15 @@
 'use client';
+import React, { useState } from 'react';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+
 import AuthForm from '@/components/AuthForm';
-import React from 'react';
+import { useRouter } from 'next/navigation';
 
 const Login = () => {
+	const [error, setError] = useState('');
+
+	const router = useRouter();
+
 	const handleSubmit = async (
 		e: React.FormEvent,
 		email: string,
@@ -10,12 +17,24 @@ const Login = () => {
 	) => {
 		e.preventDefault();
 		console.log(email, password, 'user login ');
+		setError('');
+		const supabase = createClientComponentClient();
+		const { error } = await supabase.auth.signInWithPassword({
+			email,
+			password,
+		});
+		if (error) {
+			setError(error.message);
+			return;
+		}
+		router.push('/');
 	};
 
 	return (
 		<main className="flex flex-col gap-5 items-center justify-center h-screen">
 			<h2 className="text-center text-3xl font-semibold">Login</h2>
 			<AuthForm handleSubmit={handleSubmit} />
+			{error && <p className="text-2xl font-bold text-red-500">{error}</p>}
 		</main>
 	);
 };
